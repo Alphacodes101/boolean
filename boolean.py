02309967babb4b49005ef311c7d37948cb0e986f
Alphacodes <84385204+Alphacodes101@users.noreply.github.com>
2021-09-22 16:23:32 +0100

    boolean in python

---
+++ boolean.py
@@ -0,0 +1,37 @@
+# Boolean Values
+# print(8 >  9)
+# print(8== 9)
+# print(12< 11)
+
+
+# # Evaluate Values and Variables
+# print(bool("Hello"))
+# print(bool("rhy"))
+
+# # Most Values are True
+# # Almost any value is evaluated to True if it has some sort of content.
+# # Any string is True, except empty strings.
+# # Any number is True, except 0.
+# # Any list, tuple, set, and dictionary are True, except empty ones.
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+
+# # Some Values are False
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+# # Functions can Return a Boolean
+# def myFunction() :
+#       return True
+
+# print(myFunction())
+
+
